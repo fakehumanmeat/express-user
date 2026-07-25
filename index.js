de54b12dbebf7a3fa596988be60125c48c429385
Boi <cambodianjesuss@gmail.com>
2026-07-25 14:09:33 -0700

    static files

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// middleware static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// get index.html file
 app.get('/', (req, res) => {
-  res.send('Hello World!! new branch');
+    res.sendFile(index.html);
 } );
 
 app.listen(port, () => {
